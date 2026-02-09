18e1b7fb570f8b92031c050a8e5dc3412fd88e78
Conil11 <galeriaconil@hotmail.com>
2026-02-10 00:10:31 +0100

    Create page.jsx

---
+++ app/page.jsx
@@ -0,0 +1,17 @@
+export default function Home() {
+  return (
+    <main style={{ textAlign: "center", padding: "80px 20px" }}>
+      <h1 style={{ fontSize: "48px" }}>Galería Conil</h1>
+      <p>Arte contemporáneo · Tanger</p>
+
+      <img
+        src="https://images.unsplash.com/photo-1577720580479-7d839d829c73"
+        style={{ width: "100%", maxWidth: 900, marginTop: 40 }}
+      />
+
+      <p style={{ marginTop: 40 }}>
+        Web profesional en construcción.
+      </p>
+    </main>
+  );
+}
